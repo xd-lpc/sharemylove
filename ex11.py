145bb677a328da3889d5f4f05a91e26042ca0854
xd-lpc <xdlpc@gmail.com>
2018-05-01 00:39:12 +0800

    learn python hard way

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print ("How old are you")
+age = input()
+print ("How tall are you")
+tall = input()
+print ("How much do you weigh")
+weigh = input()
+
+print ("So, you are %r years old, %r tall and %r heavy. " % (age,tall,weigh))
+
+#------------------------ex12
+
+Name = input("Name?")
+print (Name)
---
+++ ex9.py
@@ -0,0 +1,15 @@
+#Here is some new strange stuff, remreber type it exactly
+
+days = "Mod Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days :" ,days)
+print ("Here are the months: " , months)
+
+print("""
+There is something going on here
+with the three double-quotes.
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6
+
+""")
