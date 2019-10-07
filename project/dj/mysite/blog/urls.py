72b202b33c9a956a74ed2420be8a068748607f63
xd-lpc <xdlpc@gmail.com>
2019-10-07 13:31:59 +0800

    add register page

--- project/dj/mysite/blog/urls.py
+++ project/dj/mysite/blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('^post/new/$', views.post_new, name='post_new'),
+    path('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
